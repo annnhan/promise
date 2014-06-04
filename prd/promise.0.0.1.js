8f27a84898436e101a7c92670a2a7747d85cdcad
an.han <an.han@qunar.com>
2014-06-04 19:18:52 +0800

    in

--- prd/promise.0.0.1.js
+++ prd/promise.0.0.1.js
@@ -1,52 +1,46 @@
-/**
- * Created by an.han on 14-6-4.
- */
 ~function (win) {
-    
-    // constructor
-    function Promise (fun) {
-        var me = this;
-        var resolve = function (val) {
-            me.resolve(val);
-        }
-        var reject = function (val) {
-            me.reject(val);
-        }
+    var Promise = function (fun) {
+            var me = this,
+                resolve = function (val) {
+                    me.resolve(val);
+                },
+                reject = function (val) {
+                    me.reject(val);
+                }
+            me.st = 'pending';
+            me.rsq = [];
+            me.rjq = [];
+            (typeof fun === 'function') && fun(resolve, reject);
+        },
+        fn = Promise.prototype;
 
-        me.st = 'default';
-        me.rsq = [];
-        me.rjq = [];
-
-        (typeof fun === 'function') && fun(resolve, reject);
-
-    }
-    
-    Promise.fn = Promise.prototype;
-
-    Promise.fn.then = function (resolve, reject) {
+    fn.then = function (resolve, reject) {
         this.rsq.push(resolve);
         this.rjq.push(reject);
         return this;
     }
 
-    Promise.fn.resolve = function (val) {
-        if (this.st === 'resolved' || this.st === 'default' ) {
+    fn.catch = function (reject) {
+        return this.then(null, reject);
+    }
+
+    fn.resolve = function (val) {
+        if (this.st === 'resolved' || this.st === 'pending') {
             this.st = 'resolved';
             this._doQ(val);
         }
-        
     }
 
-    Promise.fn.reject = function (val) {
-        if (this.st === 'rejected' || this.st === 'default') {
+    fn.reject = function (val) {
+        if (this.st === 'rejected' || this.st === 'pending') {
             this.st = 'rejected';
             this._doQ(val);
         }
     }
 
-    Promise.fn._doQ = function (val) {
+    fn._doQ = function (val) {
         if (!this.rsq.length && !this.rjq.length) {
-            return ;
+            return;
         }
 
         var resolve = this.rsq.shift(),
@@ -59,15 +53,16 @@
         if (this.st === 'rejected' && typeof reject === 'function') {
             ret = reject(val);
         }
-        if (ret instanceof Promise) {
-            ret.rsq = this.rsq;
-            ret.rjq = this.rjq;
-            this.rsq = [];
-            this.rjq = [];
+        if (!(ret instanceof Promise)) {
+            var _ret = ret;
+            ret = new Promise(function (resolve) {
+                setTimeout(function () {
+                    resolve(_ret);
+                }, 0);
+            });
         }
-        else {
-            this._doQ(ret);
-        }
+        ret.rsq = this.rsq.splice(0);
+        ret.rjq = this.rjq.splice(0);
     }
 
     Promise.all = function (arr) {
@@ -75,14 +70,14 @@
         var len = arr.length,
             i = 0,
             res = 0;
-        while(i < len) {
+        while (i < len) {
             arr[i].then(
-                function(i){ return function () {
-                    if(++res === len){
+                function () {
+                    if (++res === len) {
                         pms.resolve();
                     }
-            }}(i),
-                function(val){
+                },
+                function (val) {
                     pms.reject(val);
                 }
             );
@@ -91,7 +86,23 @@
         return pms;
     }
 
+    Promise.resolve = function (obj) {
+        var pms = new Promise();
+        if (obj && typeof obj.then === 'function') {
+            for (var i in pms) {
+                obj[i] = pms[i];
+            }
+            return obj;
+        }
+        else {
+            setTimeout(function () {
+                pms.resolve(obj);
+            }, 0);
+            return pms;
+        }
+
+    }
+
     win.Promise = Promise;
 
 }(window);
-
